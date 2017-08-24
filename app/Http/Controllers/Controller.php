adf5bcb9b62fd3d19de0057c0d9e5caec337f124
Lan Dong Si Hoang (Geraint Dong) <lan.dong@sparkxlab.com>
2017-08-24 16:40:00 +0700

    Clean controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -18,48 +17,15 @@ class Controller extends BaseController
         $url = $request->get('url');
 
         if ($url) {
-            // $headers = ["Content-Type:multipart/form-data"];
-
             // initialise the curl request
             $request = curl_init($url);
 
-            // prepare the request
-            // curl_setopt($request, CURLOPT_POST, true);
-            /*
-            curl_setopt(
-                $request,
-                CURLOPT_POSTFIELDS,
-                [
-                    'uploaded_file' => (new \CURLFile($file_path)),
-                    'account' => config('services.textkernel.account'),
-                    'username' => config('services.textkernel.username'),
-                    'password' => config('services.textkernel.password'),
-                ]
-            );
-            */
-            // curl_setopt($request, CURLOPT_HEADER, $headers);
             curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
 
             // Send the request
             $response = curl_exec($request);
 
             return $response;
-
-            $header_size = curl_getinfo($request, CURLINFO_HEADER_SIZE);
-            $responseCode = curl_getinfo($request, CURLINFO_HTTP_CODE);
-
-            // output the response
-            $header = substr($response, 0, $header_size);
-            $result = substr($response, $header_size);
-            dd($response);
-            if ($responseCode != Response::HTTP_OK) {
-                throw new \Exception(strip_tags($result));
-            }
-
-            // destroy resources
-            curl_close($request);
-
-            return (json_encode(simplexml_load_string($result)));
         }
 
         abort(500);
